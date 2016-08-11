a04baaddbae55aaa26420cf2172a3846eb196eb1
Hagai Cohen <hagai.co@gmail.com>
2016-08-11 19:31:19 +0300

    Added more testing

--- test/test.ts
+++ test/test.ts
@@ -2,6 +2,7 @@
 
 import { readFile,
          read,
+         write,
          utils } from "..";
 import { expect, should } from "chai";
 
@@ -12,7 +13,13 @@ describe("Example", () => {
     it("Should be pass sanity", (done) => {
         expect(readFile).to.be.a("function");
         expect(read).to.be.a("function");
+        expect(write).to.be.a("function");
         expect(utils).to.be.a("Object");
+        expect(utils.sheet_to_json).to.be.a("function");
+        expect(utils.sheet_to_csv).to.be.a("function");
+        expect(utils.sheet_to_formulae).to.be.a("function");
+        expect(utils.encode_cell).to.be.a("function");
+        expect(utils.encode_range).to.be.a("function");
         done();
     });
 });
